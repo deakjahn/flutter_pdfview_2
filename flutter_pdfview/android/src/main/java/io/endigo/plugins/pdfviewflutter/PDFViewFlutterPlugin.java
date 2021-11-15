c3697da161030933d1d2a6c690004d0d0774e050
deakjahn <deakjahn@gmail.com>
2021-11-15 21:24:13 +0100

    v2 embedding

--- flutter_pdfview/android/src/main/java/io/endigo/plugins/pdfviewflutter/PDFViewFlutterPlugin.java
+++ flutter_pdfview/android/src/main/java/io/endigo/plugins/pdfviewflutter/PDFViewFlutterPlugin.java
@@ -1,13 +1,28 @@
 package io.endigo.plugins.pdfviewflutter;
 
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+import androidx.annotation.NonNull;
 
-public class PDFViewFlutterPlugin {
-    /** Plugin registration. */
-    public static void registerWith(Registrar registrar) {
-        registrar
-                .platformViewRegistry()
-                .registerViewFactory(
-                        "plugins.endigo.io/pdfview", new PDFViewFactory(registrar.messenger()));
-    }
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+//import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+public class PDFViewFlutterPlugin implements FlutterPlugin {
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+    binding
+      .getPlatformViewRegistry()
+      .registerViewFactory("plugins.endigo.io/pdfview", new PDFViewFactory(binding.getBinaryMessenger()));
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+  }
+
+//  public static void registerWith(Registrar registrar) {
+//    registrar
+//      .platformViewRegistry()
+//      .registerViewFactory(
+//        "plugins.endigo.io/pdfview", new PDFViewFactory(registrar.messenger()));
+//  }
+
 }
